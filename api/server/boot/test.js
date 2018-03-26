6156f2ba2128b865e1b338886cee42ba48b5b790
dazzed <sameepsheth@gmail.com>
2018-03-26 13:12:25 -0400

    remove test code

--- api/server/boot/test.js
+++ api/server/boot/test.js
@@ -1,25 +1,7 @@
 module.exports = async function (server) {
   try {
     // test code here
-    const {user, accessToken} = server.models;
-    var pattern = new RegExp('.*'+'gmail'+'.*', "i");
-    const res = await user.find({
-      where: {
-        or: [
-          {
-            firstName: {
-              ilike: '%1%'
-            }
-          },
-          {
-            lastName: {
-              ilike: '%1%'
-            }
-          }
-        ]
-      }
-    });
-    log(res)
+    // const {user, accessToken} = server.models;
   } catch (error) {
     console.log(error);
     throw error;
