6081e4d503f654194ed8f633723e94ffae479da6
Anthony Lobko <anthonylobko@gmail.com>
2018-04-16 12:23:30 -0400

    fixed eslint issues

--- api/server/models/asset/updateAssetRates.js
+++ api/server/models/asset/updateAssetRates.js
@@ -25,126 +25,125 @@ const _ = require('lodash');
 // dash
 // ada
 const krakenMap = {
-    'BCHUSD': ['bch', 'usd'],
-    'BCHXBT': ['bch', 'btc'],
-    'DASHUSD': ['dash', 'usd'],
-    'DASHXBT': ['dash', 'btc'],
-    'XETHXXBT': ['eth', 'btc'],
-    'XETHZUSD': ['eth', 'usd'],
-    'XLTCXXBT': ['ltc', 'btc'],
-    'XLTCZUSD': ['ltc', 'usd'],
-    'XXBTZUSD': ['btc', 'usd'],
-    'XXLMXXBT': ['xlm', 'btc'],
-    'XXMRXXBT': ['xmr', 'btc'],
-    'XXMRZUSD': ['xmr', 'usd'],
-    'XXLMZUSD': ['xlm', 'usd'],
-    'XXRPXXBT': ['xrp', 'btc'],
-    'XXRPZUSD': ['xrp', 'usd'],
-    'XZECXXBT': ['zec', 'btc'],
-    'XZECZUSD': ['zec', 'usd']
+  'BCHUSD': ['bch', 'usd'],
+  'BCHXBT': ['bch', 'btc'],
+  'DASHUSD': ['dash', 'usd'],
+  'DASHXBT': ['dash', 'btc'],
+  'XETHXXBT': ['eth', 'btc'],
+  'XETHZUSD': ['eth', 'usd'],
+  'XLTCXXBT': ['ltc', 'btc'],
+  'XLTCZUSD': ['ltc', 'usd'],
+  'XXBTZUSD': ['btc', 'usd'],
+  'XXLMXXBT': ['xlm', 'btc'],
+  'XXMRXXBT': ['xmr', 'btc'],
+  'XXMRZUSD': ['xmr', 'usd'],
+  'XXLMZUSD': ['xlm', 'usd'],
+  'XXRPXXBT': ['xrp', 'btc'],
+  'XXRPZUSD': ['xrp', 'usd'],
+  'XZECXXBT': ['zec', 'btc'],
+  'XZECZUSD': ['zec', 'usd']
 };
 const nonBTCETHAssets = [
-    'xrp',
-    'bch',
-    'ltc',
-    'xlm',
-    'xmr',
-    'xmr',
-    'dash',
-    'ada'
+  'xrp',
+  'bch',
+  'ltc',
+  'xlm',
+  'xmr',
+  'xmr',
+  'dash',
+  'ada'
 ];
 
 const krakenData = async () => {
-    let krakenData = {};
-    const krakData = await kraken.api('Ticker', { pair: 'BCHUSD,BCHXBT,DASHUSD,DASHXBT,XETHXXBT,XETHZUSD,XLTCXXBT,XLTCZUSD,XXBTZUSD,XXLMXXBT,XXLMZUSD,XXMRXXBT,XXMRZUSD,XXRPXXBT,XXRPZUSD,XZECXXBT,XZECZUSD'});
-    const keys = Object.keys(krakenMap);
-    for(const key in keys){
-        const properKey = keys[key];
-        const tickerPair = krakData.result[properKey];
-        const firstKey = krakenMap[properKey][0];
-        const secondKey = krakenMap[properKey][1];
-        if(!(firstKey in krakenData))
-            krakenData[firstKey] = {};
-        krakenData[firstKey][secondKey] = {
-            bid: tickerPair.b[0],
-            ask: tickerPair.a[0],
-            price: tickerPair.c[0]
-        };
-
-    }
-    return krakenData;
-}
+  let krakenData = {};
+  const krakData = await kraken.api('Ticker', { pair: 'BCHUSD,BCHXBT,DASHUSD,DASHXBT,XETHXXBT,XETHZUSD,XLTCXXBT,XLTCZUSD,XXBTZUSD,XXLMXXBT,XXLMZUSD,XXMRXXBT,XXMRZUSD,XXRPXXBT,XXRPZUSD,XZECXXBT,XZECZUSD'});
+  const keys = Object.keys(krakenMap);
+  for (const key in keys) {
+    const properKey = keys[key];
+    const tickerPair = krakData.result[properKey];
+    const firstKey = krakenMap[properKey][0];
+    const secondKey = krakenMap[properKey][1];
+    if (!(firstKey in krakenData))
+      krakenData[firstKey] = {};
+    krakenData[firstKey][secondKey] = {
+      bid: tickerPair.b[0],
+      ask: tickerPair.a[0],
+      price: tickerPair.c[0]
+    };
+  }
+  return krakenData;
+};
 
 const getBinanceKeys = ticker => {
-    let firstTicker = '';
-    let secondTicker = '';
-    if(ticker.startsWith('BTC') || ticker.startsWith('ETH') || ticker.startsWith('LTC')){
-        firstTicker = ticker.slice(0, 3).toLowerCase();
-        secondTicker = ticker.slice(3, 10).toLowerCase();
-    }else if(ticker.endsWith('BTC') || ticker.endsWith('ETH')){
-        firstTicker = ticker.slice(0, -3).toLowerCase();
-        secondTicker = ticker.slice(-3).toLowerCase();
-    }else{
-        return null;
-    }
-    if(firstTicker==='usdt'){
-        firstTicker = 'usd';
-    }
-    if(secondTicker==='usdt'){
-        secondTicker = 'usd';
-    }
-    return [firstTicker, secondTicker];
-}
+  let firstTicker = '';
+  let secondTicker = '';
+  if (ticker.startsWith('BTC') || ticker.startsWith('ETH') || ticker.startsWith('LTC')) {
+    firstTicker = ticker.slice(0, 3).toLowerCase();
+    secondTicker = ticker.slice(3, 10).toLowerCase();
+  } else if (ticker.endsWith('BTC') || ticker.endsWith('ETH')) {
+    firstTicker = ticker.slice(0, -3).toLowerCase();
+    secondTicker = ticker.slice(-3).toLowerCase();
+  } else {
+    return null;
+  }
+  if (firstTicker === 'usdt') {
+    firstTicker = 'usd';
+  }
+  if (secondTicker === 'usdt') {
+    secondTicker = 'usd';
+  }
+  return [firstTicker, secondTicker];
+};
 
 const binanceData = async () => {
-    let binanceDats = {};
-    const binancePrices = await binanceClient.prices()
-    const binanceBidAsk = await binanceClient.allBookTickers()
-    const tickers = _.keys(binancePrices);
-    const tickersIncluded = _.filter(tickers, ticker => {
-        return (ticker.startsWith('ETH') || ticker.startsWith('BTC') || ticker.endsWith('ETH') || ticker.endsWith('BTC') || ticker.endsWith('USDT') || ticker.endsWith('USDT')) && nonBTCETHAssets.some(tick=>{
-            const lowerCase = ticker.toLowerCase();
-            return lowerCase.includes(tick);
-        });
+  let binanceDats = {};
+  const binancePrices = await binanceClient.prices();
+  const binanceBidAsk = await binanceClient.allBookTickers();
+  const tickers = _.keys(binancePrices);
+  const tickersIncluded = _.filter(tickers, ticker => {
+    return (ticker.startsWith('ETH') || ticker.startsWith('BTC') || ticker.endsWith('ETH') || ticker.endsWith('BTC') || ticker.endsWith('USDT') || ticker.endsWith('USDT')) && nonBTCETHAssets.some(tick=>{
+      const lowerCase = ticker.toLowerCase();
+      return lowerCase.includes(tick);
     });
-    const fullTickersToInclude = ['BTCUSDT', 'ETHUSDT', ...tickersIncluded];
-    const prices = _.pick(binancePrices, fullTickersToInclude);
-    const bidAsks = _.filter(binanceBidAsk, elem => {
-        return fullTickersToInclude.some(tick => {
-            return tick===elem.symbol;
-        });
+  });
+  const fullTickersToInclude = ['BTCUSDT', 'ETHUSDT', ...tickersIncluded];
+  const prices = _.pick(binancePrices, fullTickersToInclude);
+  const bidAsks = _.filter(binanceBidAsk, elem => {
+    return fullTickersToInclude.some(tick => {
+      return tick === elem.symbol;
     });
-    for(const index in bidAsks){
-        const elem = bidAsks[index];
-        const symbol = elem.symbol;
-        const keys = getBinanceKeys(symbol);
-        const price = prices[symbol];
-        _.set(binanceDats, [ ...keys, 'price'], price);
-        _.set(binanceDats, [ ...keys, 'bid'], elem.bidPrice);
-        _.set(binanceDats, [ ...keys, 'ask'], elem.askPrice);
-    }
-    return binanceDats;
-}
+  });
+  for (const index in bidAsks) {
+    const elem = bidAsks[index];
+    const symbol = elem.symbol;
+    const keys = getBinanceKeys(symbol);
+    const price = prices[symbol];
+    _.set(binanceDats, [...keys, 'price'], price);
+    _.set(binanceDats, [...keys, 'bid'], elem.bidPrice);
+    _.set(binanceDats, [...keys, 'ask'], elem.askPrice);
+  }
+  return binanceDats;
+};
 
 module.exports = async function(app) {
-    const assets = await app.models.asset.find({});
-    const krakDat = await krakenData();
-    const binanceDat = await binanceData();
-    const mergedData = _.mergeWith(krakDat, binanceDat, (objValue, srcValue, key) => {
-        if(key==='ask'){
-            return objValue < srcValue ? objValue : srcValue;
-        }else if(key === 'bid'){
-            return objValue > srcValue ? objValue : srcValue;
-        }else if(key === 'price'){
-            return objValue > srcValue ? objValue : srcValue;
-        }else{
-            return undefined;
-        }
-    });
-    for(assetIndex in assets){
-        const currentAsset = assets[assetIndex];
-        const exchangeRates = currentAsset.exchangeRates;
-        const newRates = _.merge(exchangeRates, mergedData[currentAsset.ticker]);
-        await currentAsset.updateAttribute('exchangeRates', newRates);
+  const assets = await app.models.asset.find({});
+  const krakDat = await krakenData();
+  const binanceDat = await binanceData();
+  const mergedData = _.mergeWith(krakDat, binanceDat, (objValue, srcValue, key) => {
+    if (key === 'ask') {
+      return objValue < srcValue ? objValue : srcValue;
+    } else if (key === 'bid') {
+      return objValue > srcValue ? objValue : srcValue;
+    } else if (key === 'price') {
+      return objValue > srcValue ? objValue : srcValue;
+    } else {
+      return undefined;
     }
+  });
+  for (assetIndex in assets) {
+    const currentAsset = assets[assetIndex];
+    const exchangeRates = currentAsset.exchangeRates;
+    const newRates = _.merge(exchangeRates, mergedData[currentAsset.ticker]);
+    await currentAsset.updateAttribute('exchangeRates', newRates);
+  }
 };
--- api/server/models/wallet/hooks.js
+++ api/server/models/wallet/hooks.js
@@ -8,91 +8,88 @@ const uuidv4 = require('uuid/v4');
 const loopback = require('loopback');
 const priceConvert = require('../asset/priceConversion');
 
-const loaded = async (context) => {
-    var bitgo = new BitGoJS.BitGo({ env: 'test', accessToken: process.env.BITGO_API_KEY });
-    if(!context.data.assetId){
-
-        return true;
-    }
-    if(context.data.assetId==='eth' && !context.data.address.startsWith('0x') && !!context.data.address && context.data.address!=="\"\""){
-        const wallet = await bitgo.coin('teth').wallets().get({ id: process.env.ETH_WALLET});
-        const address = await wallet.getAddress({address: context.data.address});
-        if(address.address){
-            setTimeout(async () => {
-                const instance = await Wallet.findOne({ where: { id: context.data.id } });
-                await instance.updateAttribute('address', address.address);
-            }, 1000);
-            context.data.address = address.address;
-        }
-    }
-    if(context.data.assetId==='eth'){
-        context.data.usdPrice = await priceConvert.price(context.data.balance, 'eth', 'usd');
-        context.data.btcPrice = await priceConvert.price(context.data.balance, 'eth', 'btc');
-        context.data.ethPrice = parseFloat(context.data.balance);
-    }else if(context.data.assetId==='btc'){
-        context.data.usdPrice = await priceConvert.price(context.data.balance, 'btc', 'usd');
-        context.data.ethPrice = await priceConvert.price(context.data.balance, 'btc', 'eth');
-        context.data.btcPrice = parseFloat(context.data.balance);
-    }else{
-        context.data.btcPrice = await priceConvert.price(context.data.balance, context.data.assetId, 'btc');
-        context.data.ethPrice = await priceConvert.price(context.data.balance, context.data.assetId, 'eth');
-        context.data.usdPrice = await priceConvert.price(context.data.balance, context.data.assetId, 'usd');
-    }
+const loaded = async (context, Wallet) => {
+  var bitgo = new BitGoJS.BitGo({ env: 'test', accessToken: process.env.BITGO_API_KEY });
+  if (!context.data.assetId) {
     return true;
-}
+  }
+  if (context.data.assetId === 'eth' && !context.data.address.startsWith('0x') && !!context.data.address && context.data.address !== '""') {
+    const wallet = await bitgo.coin('teth').wallets().get({ id: process.env.ETH_WALLET});
+    const address = await wallet.getAddress({address: context.data.address});
+    if (address.address) {
+      setTimeout(async () => {
+        const instance = await Wallet.findOne({ where: { id: context.data.id } });
+        await instance.updateAttribute('address', address.address);
+      }, 1000);
+      context.data.address = address.address;
+    }
+  }
+  if (context.data.assetId === 'eth') {
+    context.data.usdPrice = await priceConvert.price(context.data.balance, 'eth', 'usd');
+    context.data.btcPrice = await priceConvert.price(context.data.balance, 'eth', 'btc');
+    context.data.ethPrice = parseFloat(context.data.balance);
+  } else if (context.data.assetId === 'btc') {
+    context.data.usdPrice = await priceConvert.price(context.data.balance, 'btc', 'usd');
+    context.data.ethPrice = await priceConvert.price(context.data.balance, 'btc', 'eth');
+    context.data.btcPrice = parseFloat(context.data.balance);
+  } else {
+    context.data.btcPrice = await priceConvert.price(context.data.balance, context.data.assetId, 'btc');
+    context.data.ethPrice = await priceConvert.price(context.data.balance, context.data.assetId, 'eth');
+    context.data.usdPrice = await priceConvert.price(context.data.balance, context.data.assetId, 'usd');
+  }
+  return true;
+};
 
 const observe = async(context) => {
-    if(!context.query.where)
-        context.query.where = {};
-    if(context.options && context.options.accessToken){
-        const userID = context.options.accessToken.userId;
-        const user = await Wallet.app.models.user.findOne({where: {id: userID}});
-        if(!(await user.isAdmin() || await user.isSuperAdmin())){
-            context.query.where.userId = userID;
-        }
+  if (!context.query.where)
+    context.query.where = {};
+  if (context.options && context.options.accessToken) {
+    const userID = context.options.accessToken.userId;
+    const user = await Wallet.app.models.user.findOne({where: {id: userID}});
+    if (!(await user.isAdmin() || await user.isSuperAdmin())) {
+      context.query.where.userId = userID;
     }
-    return true;
-}
+  }
+  return true;
+};
 
 module.exports = function (Wallet) {
-    Wallet.beforeRemote('create', async (context, _, next) => {
-        const assetId = context.args.data.assetId;
-        const userId = context.args.options.accessToken.userId;
-        context.args.data.userId = userId;
-        const foundWallet = await Wallet.findOne({ where: { userId: userId, assetId: assetId } });
-        if (foundWallet) {
-            return next(badRequest('Already have a wallet of this type.'));
+  Wallet.beforeRemote('create', async (context, _, next) => {
+    const assetId = context.args.data.assetId;
+    const userId = context.args.options.accessToken.userId;
+    context.args.data.userId = userId;
+    const foundWallet = await Wallet.findOne({ where: { userId: userId, assetId: assetId } });
+    if (foundWallet) {
+      return next(badRequest('Already have a wallet of this type.'));
+    }
+    var bitgo = new BitGoJS.BitGo({ env: 'test', accessToken: process.env.BITGO_API_KEY });
+    if (assetId === 'btc' || assetId == 'eth') {
+      const testCoin = 't' + assetId;
+      try {
+        const wallet = await bitgo.coin(testCoin).wallets().get({ id: testCoin === 'tbtc' ? process.env.BTC_WALLET : process.env.ETH_WALLET });
+        const address = await wallet.createAddress({ label: uuidv4() });
+        if (assetId === 'btc') {
+          context.args.data.address = address.address;
+        } else {
+          context.args.data.address = address.id;
         }
-        var bitgo = new BitGoJS.BitGo({ env: 'test', accessToken: process.env.BITGO_API_KEY });
-        if (assetId === 'btc' || assetId == 'eth') {
-            const testCoin = "t" + assetId;
-            try {
-                const wallet = await bitgo.coin(testCoin).wallets().get({ id: testCoin === 'tbtc' ? process.env.BTC_WALLET : process.env.ETH_WALLET });
-                const address = await wallet.createAddress({ label: uuidv4() });
-                if (assetId === 'btc') {
-                    context.args.data.address = address.address;
-                } else {
-                    context.args.data.address = address.id;
-                }
-            }
-            catch (e) {
-                console.log('Error creating wallet');
-            }
-        }
-
+      }            catch (e) {
+        console.log('Error creating wallet');
+      }
+    }
   });
   Wallet.observe('access', async (context, next) => {
     observe(context).then(n=>{
-        next();
+      next();
     }).catch(e=>{
-        next(e);
+      next(e);
     });
   });
   Wallet.observe('loaded', (context, next) => {
-    loaded(context).then(n=>{
-        next();
+    loaded(context, Wallet).then(n=>{
+      next();
     }).catch(e=>{
-        next(e);
+      next(e);
     });
   });
 };
