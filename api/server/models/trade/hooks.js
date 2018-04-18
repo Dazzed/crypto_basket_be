f0aae2eaebf4afc66fb4c5716e29857070a916b8
Anthony Lobko <anthonylobko@gmail.com>
2018-04-18 18:10:18 -0400

    added user access control for admins and supers

--- api/server/models/trade/hooks.js
+++ api/server/models/trade/hooks.js
@@ -2,6 +2,19 @@ const priceConvert = require('../asset/priceConversion');
 const { tradeEmail } = require('../../helpers/sendGrid');
 const _ = require('lodash');
 
+const observe = async (context, Trade) => {
+    if (!context.query.where)
+        context.query.where = {};
+    if (context.options && context.options.accessToken) {
+        const userID = context.options.accessToken.userId;
+        const user = await Trade.app.models.user.findOne({ where: { id: userID } });
+        if (!(await user.isAdmin() || await user.isSuperAdmin())) {
+            context.query.where.userId = userID;
+        }
+    }
+    return true;
+}
+
 module.exports = function(Trade) {
     Trade.initiateTrade = async (context, request, response, fromAssetId, toAssetId, fromAssetAmount, toAssetAmount, tradeType) => {
         const userId = request.accessToken.userId;
@@ -219,5 +232,17 @@ module.exports = function(Trade) {
       status: 200,
       verb: 'post'
     }
+    });
+
+    Trade.observe('access', async (context, next) => {
+        if (context.options.skipAllHooks) {
+            return next();
+        } else {
+            return observe(context, Trade).then(n => {
+                return next();
+            }).catch(e => {
+                return next(e);
+            });
+        }
     });
 };
