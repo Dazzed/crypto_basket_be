b75eb42646bcdf03755409dd1dd2d1726158bc21
Anthony Lobko <anthonylobko@gmail.com>
2018-05-04 21:58:33 -0400

    added filtering to search query

--- api/server/models/user/remote_methods/search.js
+++ api/server/models/user/remote_methods/search.js
@@ -1,12 +1,14 @@
 const createRemoteMethod = require('../../../helpers/createRemoteMethod');
 const { badRequest, unauthorized, internalError } = require('../../../helpers/errorFormatter');
+const _ = require('lodash');
 
 module.exports = user => {
   createRemoteMethod({
     model: user,
     name: 'search',
     accepts: [
-      { arg: 'query', type: 'string', required: true, description: 'search query' }
+      { arg: 'query', type: 'string', required: true, description: 'search query' },
+      { arg: 'filter', type: 'object', required: false, description: 'search filter' }
     ],
     description: 'search for community members',
     httpOptions: {
@@ -17,14 +19,14 @@ module.exports = user => {
     }
   });
 
-  user.search = async (request, response, query) => {
+  user.search = async (request, response, query, filter) => {
     try {
       const { roleMapping } = user.app.models;
       // ignore privledged users in search
       const privledgedIds = await roleMapping.find({ fields: 'principalId' })
         .map(({ principalId }) => principalId);
       const fields = ['firstName', 'lastName', 'email', 'username'];
-      const results = await user.find({
+      const filterBase = {
         where: {
           and: [
             {
@@ -41,7 +43,12 @@ module.exports = user => {
             }
           ],
         }
-      });
+      };
+      let filterQuery = _.assign({}, filter, filterBase);
+      if(filter){
+        filterQuery.where.and.push(filter.where);
+      }
+      const results = await user.find(filterQuery);
       return response.status(200).send(results);
     } catch (error) {
       console.log('Error in remote method user.search ', error);
