9c0fb7b6a08a79c91aded9348d837dce168b20df
Dazzed <sameepsheth@gmail.com>
2018-07-01 08:56:13 -0400

    1. Add file name column to documents, 2.) updates to document uploading logic

--- api/server/helpers/getFileFromRequest.js
+++ api/server/helpers/getFileFromRequest.js
@@ -4,13 +4,17 @@ module.exports = function getFileFromRequest(req) {
   return new Promise((resolve, reject) => {
     const form = new multiparty.Form();
     form.parse(req, (err, fields, files) => {
-      if (err) reject(err);
-      if (!files) {
-        return reject(false);
+      try {
+        if (err) reject(err);
+        if (!files) {
+          return reject(false);
+        }
+        const file = files['file'][0]; // get the file from the returned files object
+        if (!file) Promise.reject('File was not found in form data.');
+        else resolve(file);
+      } catch (e) {
+        return reject(e);
       }
-      const file = files['file'][0]; // get the file from the returned files object
-      if (!file) Promise.reject('File was not found in form data.');
-      else resolve(file);
     });
   });
 };
